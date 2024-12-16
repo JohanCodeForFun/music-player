85bd547087c6a9c15497671eea57b3142b9e83d1
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-12-16 08:19:58 +0100

    refactor meme store to setup

--- src/stores/memeStore.ts
+++ src/stores/memeStore.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import { ref } from 'vue';
 
 export type Meme = {
   id: string;
@@ -6,23 +7,48 @@ export type Meme = {
   url: string;
 };
 
-export const useMemeStore = defineStore('memeStore', {
-  state: () => ({
-    memes: [] as Meme[],
-    loading: false,
-  }),
-  actions: {
-    async fetchMemes() {
-      this.loading = true;
-      try {
-        const response = await fetch('https://api.imgflip.com/get_memes');
-        const data = await response.json();
-        this.memes = data.data?.memes
-      } catch (error) {
-        console.error('Error fetching memes:', error);
-      } finally {
-        this.loading = false;
-      }
-    },
-  },
-});
+export const useMemeStore = defineStore('memeStore', () => {
+  let memes = ref<Meme[]>([]);
+  let loading = ref(false);
+
+  const fetchMemes = async () => {
+    loading.value = true;
+
+    try {
+      const response  = await fetch('https://api.imgflip.com/get_memes');
+      const data = await response.json();
+      memes.value = data.data?.memes;
+    } catch (error) {
+      console.error('Error fetching memes:', error)
+    } finally {
+      loading.value = false
+    }
+  }
+  
+  return {
+    memes, 
+    loading,
+    fetchMemes
+  }
+})
+
+// export const useMemeStore = defineStore('memeStore', {
+//   state: () => ({
+//     memes: [] as Meme[],
+//     loading: false,
+//   }),
+//   actions: {
+//     async fetchMemes() {
+//       this.loading = true;
+//       try {
+//         const response = await fetch('https://api.imgflip.com/get_memes');
+//         const data = await response.json();
+//         this.memes = data.data?.memes
+//       } catch (error) {
+//         console.error('Error fetching memes:', error);
+//       } finally {
+//         this.loading = false;
+//       }
+//     },
+//   },
+// });
